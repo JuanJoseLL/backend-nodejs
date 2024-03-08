0bb5f47b597882495ad72700cced88de13c754d9
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-03-07 22:23:20 -0500

    Add validateSchema middleware and update user route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,10 +4,13 @@ import userController from "../controllers/user.controller"
 import auth from "../middlewares/auth"
 import userSchema from "../schemas/user.schemas" 
 
+import validateSchema from "../middlewares/validateSchema"
+
+
 const routes = ( app: Express ) => {
-    app.get("/users", auth, userController.getUsers)
-    app.post("/users", userController.createUser)
+    app.get("/users", userController.getUsers)
     app.post('/login', userController.login)
+    app.post("/users", validateSchema(userSchema), userController.createUser)
 };
 
 export default routes;
