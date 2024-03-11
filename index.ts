b8c3bf9f51ad2fe134526925084988f99e9462bc
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-03-11 08:38:17 -0500

    por favor dios

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import routes from './src/routes';
+import { db } from "./src/config/db"
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+const port = process.env.PORT || 3000;
 routes(app);
 
+db.then(() => {
+    app.listen(port, () => {
+                console.log(`Server is running on port ${port}`)
+                }
+    );
+            }).catch((err) => console.error("Error connecting to MongoDB", err));
 export default app;
